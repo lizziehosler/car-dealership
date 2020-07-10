e8fba5c97dbedeb5f8eeacf59baba8d0e05b61bf
lizziehosler <lizzie.hosler@gmail.com>
2020-07-10 11:29:15 -0500

    Add existing file

---
+++ new_car_form.rb
@@ -0,0 +1,12 @@
+def ask_question(value)
+    puts "What is the car's #{value}?"
+    gets.chomp
+end
+year = ask_question("year")
+make = ask_question("make")
+model = ask_question("model")
+color = ask_question("color")
+value = ask_question("value")
+puts ""
+puts "You added a: "
+puts "#{year} #{color} #{make} #{model} worth approximately $#{value}"
