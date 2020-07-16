6d2ab9cec1fb39493589e0358ca8342e9d6cd3a4
lizziehosler <lizzie.hosler@gmail.com>
2020-07-16 13:18:21 -0500

    classes and attributes

--- new_car_form.rb
+++ new_car_form.rb
@@ -1,12 +1,27 @@
-def ask_question(value)
-    puts "What is the car's #{value}?"
-    gets.chomp
+puts "Let's add a car!"
+
+class Car 
+    attr_accessor :year, :color, :make, :model, :price
+    def to_s
+        "#{@year} #{@color} #{@make} #{@model} worth approximately $#{@price}"
+    end
+
+    def ask_question(value)
+        puts "What is the car's #{value}?"
+        gets.chomp
+    end
+
+    def initialize
+        @year = ask_question("year")
+        @make = ask_question("make")
+        @model = ask_question("model")
+        @color = ask_question("color")
+        @price = ask_question("price")
+    end
+   
 end
-year = ask_question("year")
-make = ask_question("make")
-model = ask_question("model")
-color = ask_question("color")
-value = ask_question("value")
-puts ""
-puts "You added a: "
-puts "#{year} #{color} #{make} #{model} worth approximately $#{value}"
+
+form1 = Car.new
+puts form1
+
+
