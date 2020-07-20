34d0b84455e6c6dc22152d4e69748a50c675b28e
lizziehosler <lizzie.hosler@gmail.com>
2020-07-20 12:01:31 -0500

    added car lot class

--- new_car_form.rb
+++ new_car_form.rb
@@ -1,10 +1,25 @@
 puts "Let's add a car!"
 
+class CarLot
+    attr_accessor :inventory 
+    def initialize
+        @inventory = []
+    end
+
+    def add_car(car)
+        @inventory.push(car)
+
+        puts "Inventory:"
+
+        @inventory.each do |car|
+            puts car
+        end
+    end
+
+end
+
 class Car 
     attr_accessor :year, :color, :make, :model, :price
-    def to_s
-        "#{@year} #{@color} #{@make} #{@model} worth approximately $#{@price}"
-    end
 
     def ask_question(value)
         puts "What is the car's #{value}?"
@@ -18,10 +33,25 @@ class Car
         @color = ask_question("color")
         @price = ask_question("price")
     end
-   
+
+    def to_s
+        "Car: #{@year} #{@color} #{@make} #{@model} worth approximately $#{@price}"
+    end
+
 end
 
-form1 = Car.new
-puts form1
+car1 = Car.new
+car2 = Car.new
+
+lot1 = CarLot.new
+lot1.add_car(car1)
+lot1.add_car(car2)
+puts "First car in inventory:"
+puts lot1.inventory[0]
+
+
+
+
+
 
 
