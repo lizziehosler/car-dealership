cc7552771800bcc21c6674a7e6aab303abfda09e
lizziehosler <lizzie.hosler@gmail.com>
2020-07-26 23:31:14 -0500

    added menu

--- car.rb
+++ car.rb
@@ -1,21 +1,21 @@
 class Car 
     attr_accessor :year, :color, :make, :model, :price
 
-    def ask_car_question(feature)
-        puts "What is the car's #{feature}?"
+    def ask_question(value)
+        puts "What is the car's #{value}?"
         gets.chomp
     end
 
     def initialize
-        @year = ask_car_question("year")
-        @make = ask_car_question("make")
-        @model = ask_car_question("model")
-        @color = ask_car_question("color")
-        @price = ask_car_question("price")
+        @year = ask_question("year")
+        @make = ask_question("make")
+        @model = ask_question("model")
+        @color = ask_question("color")
+        @price = ask_question("price")
     end
 
     def to_s
-        puts "Car: #{@year} #{@color} #{@make} #{@model} worth approximately $#{@price}"
+        "Car: #{@year} #{@color} #{@make} #{@model} worth approximately $#{@price}"
     end
 
 end
--- car_lot.rb
+++ car_lot.rb
@@ -1,18 +1,22 @@
 require_relative 'car'
+require_relative 'menu'
 
 class CarLot
-    attr_accessor :inventory
-
+    attr_accessor :inventory 
     def initialize
         @inventory = []
     end
 
-    def add_vehicle(vehicle)
-        @inventory.push(vehicle)
+    def add_car(car)
+        car = Car.new
+        @inventory.push(car)
+    end
 
+    def list_inventory
         puts "Inventory:"
-        @inventory.each do |vehicle|
-            puts vehicle
+
+        @inventory.each do |car|
+            puts car
         end
     end
 
